c1c79ead9643d3463db15ed450217d6554da67fd
adamlin120 <b04705026@ntu.edu.tw>
2019-01-02 21:42:10 +0800

    add vgg

--- src/models/net_pytorch.py
+++ src/models/net_pytorch.py
@@ -3,102 +3,144 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
+class Vgg(nn.Module):
+    def __init__(self):
+        super(Vgg, self).__init__()
+        self.conv1 = nn.Sequential(
+            conv2d_bn_relu(1, 32, 5),
+            nn.MaxPool2d(3, 2)
+        )
+        self.conv2 = nn.Sequential(
+            conv2d_bn_relu(32, 64, 3),
+            conv2d_bn_relu(64, 64, 3),
+            nn.MaxPool2d(2, 2)
+        )
+        self.conv3 = nn.Sequential(
+            conv2d_bn_relu(64, 128, 3),
+            conv2d_bn_relu(128, 128, 3),
+            nn.MaxPool2d(2, 2)
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(1, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Linear(1024, 1)
 
     def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+
+        out = F.sigmoid(x)
+
         return out
 
 
-class res_2dcnn(nn.Module):
-    def __init__(self, in_chan, dim):
-        super(res_2dcnn, self).__init__()
-        self.conv1 = conv2d_norm_act(in_chan, dim)
-        self.conv2 = conv2d_norm_act(dim, 2*dim)
-        self.resConv1 = ResidualBlock(2*dim, 2*dim)
-        self.resConv2 = ResidualBlock(2*dim, 2*dim)
-        self.final = nn.Linear(225792, 1)
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Sequential(
+            conv2d_bn_relu(1, 32, 5),
+            nn.MaxPool2d(3, 2)
+        )
+        self.conv2 = Res_block(32, 64)
+        self.conv2 = Res_block(64, 128)
 
     def forward(self, x):
-        x = F.max_pool2d(self.conv1(x), 3, 1)
-        x = F.max_pool2d(self.conv2(x), 3, 1)
-        x = self.resConv1(x)
-        x = self.resConv2(x)
-
-	# flatten
-        x = x.view(x.size(0), -1)
-        print(x.size())
-        outs = F.sigmoid(self.final(x))
-
-        return outs
-
-class SeparableConv2d(nn.Module):
-    def __init__(self,in_channels,out_channels,kernel_size=1,stride=1,padding=0,dilation=1,bias=False):
-        super(SeparableConv2d,self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding,dilation,groups=in_channels,bias=bias)
-        self.pointwise = nn.Conv2d(in_channels,out_channels,1,1,0,1,1,bias=bias)
-
-    def forward(self,x):
         x = self.conv1(x)
-        x = self.pointwise(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
         return x
 
 
-class DSCNN(nn.Module):
-    def __init__(self, in_chan, dim):
-        super(DSCNN, self).__init__()
-
-        self.dsc1 = nn.Sequential(SeparableConv2d(in_chan, 2 * dim, 3), nn.BatchNorm2d(2 * dim), nn.ReLU())
-        self.dsc2 = nn.Sequential(SeparableConv2d(2 * dim, 2 * dim, 3, padding=1), nn.BatchNorm2d(2 * dim), nn.ReLU())
-        self.dsc3 = nn.Sequential(SeparableConv2d(2 * dim, 4 * dim, 3), nn.BatchNorm2d(4 * dim), nn.ReLU())
-
-        self.fc1 = nn.Linear(541696, 1)
+class Inception(nn.Module):
+    def __init__(self):
+        super(Inception, self).__init__()
+        self.block1 = Incep_res_block(1, 32, 5)
+        self.block2 = Incep_res_block(32, 64, 3)
+        self.block3 = Incep_res_block(64, 128, 3)
 
     def forward(self, x):
-        x = self.dsc1(x)
-        res = x
-        x = self.dsc2(x)
-        x = res + x
-        x = self.dsc3(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
 
-        x = x.view(x.size(0), -1)
-        outs = F.sigmoid(self.fc1(x))
-
-        return outs
+        return x
 
 
-def conv2d_norm_act(in_dim, out_dim, kernel_size=3, stride=1, padding=0, bias=True,
-                    norm=nn.BatchNorm2d, act=nn.ReLU):
+def conv2d_bn_relu(in_channels, out_channels, kernel_size,
+                   stride=1, padding=0, dilation=1, groups=1, bias=True,
+                   norm2d_fn=nn.BatchNorm2d, actication_fn=nn.ReLU()):
+
     return nn.Sequential(
-        nn.Conv2d(in_dim, out_dim, kernel_size, stride, padding, bias=True),
-        norm(out_dim),
-        act())
+        nn.Conv2d(in_channels, out_channels, kernel_size,
+                  stride, padding, dilation, groups, bias),
+        norm2d_fn(out_channels),
+        actication_fn
+    )
+
+
+class Res_block(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Res_block, self).__init__()
+        self.conv1 = conv2d_bn_relu(in_channels, out_channels, 3, padding=2)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=2)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.conv3 = nn.Conv2d(in_channels, out_channels, 1)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        res = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+
+        res = self.conv3(x)
+        res = self.bn3(x)
+
+        x = x + res
+
+        return x
+
+
+class Incep_res_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(Incep_res_block, self).__init__()
+        self.conv_a1 = nn.Conv2d(in_channels, 128, 1)
+        self.conv_a2 = nn.Conv2d(128, 160, (1, kernel_size))
+        self.conv_a3 = nn.Conv2d(160, 192, (kernel_size, 1))
+
+        self.conv_b1 = nn.Conv2d(in_channels, 192, 1)
+
+        self.conv_c1 = nn.Conv2d(2*192, out_channels, 1)
+
+        self.conv_d1 = nn.Conv2d(in_channels, out_channels, 1)
+
+    def forward(self, x):
+        x_a = self.conv_a1(x)
+        x_a = self.conv_a2(x_a)
+        x_a = self.conv_a3(x_a)
+
+        x_b = self.conv_b1(x)
+
+        x_c = torch.cat((x_a, x_b), 3)
+        x_c = self.conv_c1(x_c)
+
+        x = self.conv_d1(x)
+
+        out = F.relu(x + x_c)
+
+        return out
 
 
 def pred_to_01(pred):
     pred_class = (np.sign(pred.detach().clone().cpu()-0.5)+1)/2
     pred_class[pred_class == 0.5] = 1
     return pred_class
-
-
-def conv3x3(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
-                     stride=stride, padding=1, bias=False)
